0c21ae4a0f49b4b334ad7146234f29dcb51155cc
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-07-11 14:12:27 +0600

    initil folder strucure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline h-screen bg-gray-950  text-white flex items-center justify-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
