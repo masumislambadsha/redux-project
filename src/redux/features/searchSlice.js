6d8187ce918f596f89c2c3b0cd03d44869f558b1
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-07-11 19:39:07 +0600

    configured store and created searchSlice

--- src/redux/features/searchSlice.js
+++ src/redux/features/searchSlice.js
@@ -18,17 +18,29 @@ const searchSlice = createSlice({
     },
     setResults(state, action) {
       state.results = action.payload;
+      state.loading = false;
     },
     setLoading(state, action) {
       state.loading = action.payload;
+      state.error = null;
     },
     setError(state, action) {
       state.error = action.payload;
+      state.loading = false;
+    },
+    clearResults(state) {
+      state.results = [];
     },
   },
 });
 
-export const { setQuery, setActiveTabs, setError, setLoading, setResults } =
-  searchSlice.actions;
+export const {
+  setQuery,
+  setActiveTabs,
+  setError,
+  setLoading,
+  setResults,
+  clearResults,
+} = searchSlice.actions;
 
 export default searchSlice.reducer;
