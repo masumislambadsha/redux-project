0b0966ebbe88ad37c9f6f724c16ff18645889179
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-07-11 19:22:57 +0600

    added fetch apis for photos and videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { fetchPhotos } from "./api/mediaApi";
+import { fetchPhotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
-
-  function getPhotos (query) {
+  function getPhotos(query) {
     fetchPhotos(query)
       .then((data) => {
-        console.log("Unsplash response", data);
+        console.log("Unsplash response", data.results);
       })
       .catch((error) => {
         console.error("Error fetching photos", error);
       });
   }
 
+  const getVideos = (query) => {
+    fetchVideos(query)
+      .then((data) => {
+        console.log("Pexels response", data.videos);
+      })
+      .catch((error) => {
+        console.error("Error fetching photos", error);
+      });
+  };
+
   return (
-    <div className="bg-gray-800 min-h-screen flex gap-6 items-center justify-center">
+    <div className="bg-gray-800 min-h-screen flex flex-col md:flex-row gap-6 items-center justify-center">
       <h1 className="text-3xl font-bold underline   text-white flex items-center justify-center">
         Hello world!
       </h1>
-      <button onClick={() => getPhotos('cat')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center">
+      <button
+        onClick={() => getPhotos("cat")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
+      >
         Get Photos
+      </button>
+      <button
+        onClick={() => getVideos("cat")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
+      >
+        Get Videos
       </button>
     </div>
   );
